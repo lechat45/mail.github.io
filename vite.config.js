b4e0123b29499f01283fe6cb2ebd4ec6a99a7a36
Sacha <sacha@flow.app>
2026-06-09 19:43:28 +0200

    fix: rebuild frontend + fix Gmail AuthorizedHttp + fix CORS 401

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 ﻿import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-
 export default defineConfig({
   plugins: [react()],
   base: "/mail.github.io/",
-  build: {
-    outDir: "docs",
-    emptyOutDir: true
-  }
+  build: { outDir: "docs", emptyOutDir: true }
 })
